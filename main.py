e4dae92a8ba9d25c1854bbfc047307766434ce3e
Humbertho Mattar <humberthomattar@gmail.com>
2019-03-16 16:31:46 -0300

    v0.8 - diversas melhorias dos logs e parametrizacao

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import logging
 from logging.config import fileConfig
 
 #TODO: Documentacao das funcoes
-#TODO: Avaliar a necessidade do __init__
 
 
 def print_banner(texto):
     try:
         import pyfiglet
-        print(pyfiglet.figlet_format(texto))
+        logger.debug('O banner funcionara corretamente com o log level INFO.')
+        print(
+            pyfiglet.figlet_format(texto)
+            )
     except Exception as e:
         logger.erro('print_banner: ' + str(e))
     return
@@ -20,63 +22,62 @@ def print_banner(texto):
 
 def get_conf(file):
     try:
+        logger.debug('Recuperando as configuracoes da rotina => ' + str(file))
         with open(file, 'r') as ymlfile:
             cfg = yaml.safe_load(ymlfile)
         globals().update(cfg)
+        logger.debug('Configuracoes recuperadas com sucesso. => ' + str(cfg))
     except Exception as e:
         logger.error('get_conf: ' + str(e))
     return
 
-
-#TODO externalizar default_path
-def setup_logging(default_path='conf/logging.yaml', default_level=logging.INFO):
+def setup_logging(loggername, path='./conf/logging.yaml', default_level='DEBUG'):
     import coloredlogs
-    path = default_path
-    if os.path.exists(path):
-        with open(path, 'rt') as f:
-            try:
-                config = yaml.safe_load(f.read())
-                logging.config.dictConfig(config)
-                coloredlogs.install()
-            except Exception as e:
-                print(e)
-                print('Erro na configuracao do Log. Usando configuracao padrao')
-                logging.basicConfig(level=default_level)
-                coloredlogs.install(level=default_level)
-    else:
+    try:
+        global logger
+        with open(path, 'rt') as f:    
+            config = yaml.safe_load(f.read())
+            logging.config.dictConfig(config)
+            logger = logging.getLogger(loggername)
+            coloredlogs.install(logger=logger, level=logger.level)
+            logger.debug('Logger configurado corretamente. => log level: ' +  
+                str(logger.level) + ' ' + str(path))
+    except Exception as e:
+        print(str(e) + 'Erro na configuracao do Log. Usando configuracao padrao')
         logging.basicConfig(level=default_level)
         coloredlogs.install(level=default_level)
-        print('Erro na configuracao do Log. Usando configuracao padrao')
     return
 
 
-
-#TODO: Export path
-def generate_html(vars, template='templates/geral.j2'):
+def generate_html(vars, template):
+    logger.info('Iniciando a geracao do html.')
+    logger.debug(str(template) + str(vars))
     try:
         import jinja2
         path = os.path.dirname(os.path.abspath(__file__))
         env = jinja2.Environment(loader=jinja2.FileSystemLoader(path))
         html = env.get_template(template).render(vars)
+        logger.info('HTML gerado com sucesso.')
         return html
     except Exception as e:
         logger.error('generate_html: ' + str(e))
         return        
 
 
-#TODO: Export path and file
-def write_report(html, report='saida/relatorio_geral.html'):
+def write_report(html, report):
+    logger.info('Iniciando a escrita do relatorio: => ' + str(report))
     try:
         with open(report, "w") as r:
             r.write(html)
+        logger.info('Relatorio gravado com sucesso.')
         return
     except Exception as e:
         logger.error('write_report: ' + str(e))
         return
 
 
-#TODO: Export path and file
-def set_statistics(filename='data/biometria-tse-dataprev.csv'):
+def set_statistics(filename):
+    logger.info('Iniciando a analise do arquivo: ' + filename)
     try:
         import numpy as np
         import pandas as pd
@@ -84,6 +85,7 @@ def set_statistics(filename='data/biometria-tse-dataprev.csv'):
         vars = {
             "describe": df.describe().to_html()
             }
+        logger.info('Término da análise do arquivo.')
         return vars
     except Exception as e:
         logger.error('set_statistics: ' + str(e))
@@ -92,21 +94,18 @@ def set_statistics(filename='data/biometria-tse-dataprev.csv'):
 
 def main():
     try:
-        vars = set_statistics()
-        html = generate_html(vars)
-        write_report(html)
+        logger.info('Inicio do programa')
+        vars = set_statistics(input['path'] + input['filename'])
+        html = generate_html(vars, './templates/geral.j2')
+        write_report(html, (output['path'] + output['reportname']))
+        logger.info('Programa finalizado.')
     except Exception as e:
         logger.error('main: ' + str(e))
-    return
+    return 0
 
 
 if __name__ == "__main__":
-    setup_logging()
-    logger = logging.getLogger('app')
+    setup_logging(loggername='app')
     get_conf('conf/config.yaml')
-    logger.info(cfg['app'])
-    print_banner("Dtp.Labs")
+    print_banner(app['banner'])
     sys.exit(main())
-
-
-
